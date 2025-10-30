ca0636bb488066ad10939d6ac3c6998e451fca8e
Tomás Martínez <tomm.mr.98@gmail.com>
2025-10-30 18:45:56 +0100

    route-product-page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
-import { BrowserRouter, Router, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom'
 import MainLayout from './components/layout/MainLayout'
+import ProductPage from './components/pages/ProductPage'
 
 function App() {
   return (
     <BrowserRouter>
       <MainLayout>
         <Routes>
+          <Route path="/" element={<ProductPage />} />
         </Routes>
       </MainLayout>
     </BrowserRouter>

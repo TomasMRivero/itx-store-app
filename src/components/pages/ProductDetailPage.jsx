32bd33cc91a0d219565cb08fa0719cc13d375015
Tomás Martínez <tomm.mr.98@gmail.com>
2025-10-31 04:08:59 +0100

    create-product-actions-component

--- src/components/pages/ProductDetailPage.jsx
+++ src/components/pages/ProductDetailPage.jsx
@@ -3,6 +3,7 @@ import { useGetProductByIdQuery } from "../../features/product/productApi";
 import { Box, CircularProgress, Container, Grid, Paper, Typography } from "@mui/material";
 import { useParams } from "react-router-dom";
 import ProductDetail from "../product/ProductDetail";
+import ProductActions from "../product/ProductActions";
 
 const ProductDetailPage = () => {
     const { id } = useParams();
@@ -11,7 +12,12 @@ const ProductDetailPage = () => {
     if (isLoading) return <CircularProgress />;
     return(
         <Box sx={{ p: 4 }}>
-            {!isLoading && !isError && <ProductDetail product={product} />}
+            {!isLoading && !isError &&
+                <>
+                    <ProductDetail product={product} />
+                    <ProductActions options={product.options} onAddToCart={(values) => {console.log(values)}} />
+                </>
+            }
         </Box>
     )
 };
---
+++ src/components/product/ProductActions.jsx
@@ -0,0 +1,70 @@
+import { Form, Formik } from 'formik';
+import labels from '../../i18n/es.json';
+import { Box } from '@mui/system';
+import { Button, FormControl, FormHelperText, InputLabel, MenuItem, Select, Typography } from '@mui/material';
+
+const ProductActions = ({ options, onAddToCart }) => {
+    const initialValues = Object.fromEntries(Object.keys(options).map(key => {
+        const initial = options[key].length === 1 ? options[key][0].code : '';
+        return [key, initial]
+    }
+    ));
+
+    const validate = values => {
+        const errors = {};
+        Object.keys(values).forEach(key => {
+            if (!values[key]) {
+                errors[key] = labels.product.errors.options.notSelected;
+            }
+        });
+        return errors;
+    };
+
+    return (
+        <Formik
+            initialValues={initialValues}
+            validate={validate}
+            onSubmit={values => onAddToCart(values)}
+        >
+            {({ values, errors, handleSubmit, setFieldValue }) => (
+                <Form onSubmit={handleSubmit} >
+                    <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2, mt: 2 }}>
+                        {Object.keys(options).map((key) => (
+                            <>
+                            <Box key={key} p={3} borderRadius={3} sx={errors[key] ? {border: 1, borderColor: "#d32f2f"} : {}} >
+
+                                <Typography variant="subtitle1" gutterBottom>
+                                    {labels.product.options[key] || key}
+                                </Typography>
+
+                                <Box sx={{ display: 'flex', gap: 1, flexWrap: 'wrap', alignItems: 'center', justifyContent: 'center'}}>
+                                    {options[key].map((option) => (
+                                        <Button
+                                        key={option.code}
+                                        variant={values[key] === option.code ? 'contained' : 'outlined'}
+                                        onClick={() => setFieldValue(key, option.code)}
+                                        >
+                                            {option.name}
+                                        </Button>
+                                    ))}
+                                </Box>
+                            </Box>
+                            {errors[key] && (
+                                <FormHelperText error sx={{ ml:2, mt: -1 }}>
+                                    {errors[key]}
+                                </FormHelperText>
+                            )}
+                            </>
+                        ))}
+                        <Button type="submit" variant="contained" color="secondary">
+                            {labels.product.actions.addToCart}
+                        </Button>
+                    </Box>
+
+                </Form>
+            )}
+        </Formik>
+    )
+}
+
+export default ProductActions;

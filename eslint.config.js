01e4c685f5192b5ad1241dc1c743abf07d58ffe6
Tomas Martínez <tomm.mr.98@gmail.com>
2025-11-04 01:47:06 +0100

    config lint

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,7 @@ import { defineConfig, globalIgnores } from 'eslint/config'
 export default defineConfig([
   globalIgnores(['dist']),
   {
-    files: ['**/*.{js,jsx}'],
+    files: ['**/*.{js,jsx}', 'node_modules', '.eslintrc.cjs'],
     extends: [
       js.configs.recommended,
       reactHooks.configs['recommended-latest'],
